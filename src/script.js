796c32313cc2d4771c377bacf0d6941160568660
Polina Fedorova <ps.fedorova@yandex.ru>
2021-02-04 05:34:33 +0500

    refactor: edit styles

--- src/script.js
+++ src/script.js
@@ -1,17 +1,21 @@
 'use strict'
-
 import './style.css';
 import './forTests.js';
 import { data } from './data.js';
 
 const step = 5;
+const minFontValue = 5;
+const maxFontValue = 24;
+const coefficient = 100 / (maxFontValue - minFontValue);
 
 const navigationTemplate = document.querySelector('#navigation-template').content;
 const navigationList = document.querySelector('ul');
 
-const questionTemplate = document.querySelector('#question-template').content;
+const questionTemplate = document.querySelector('#question-template');
 const questionList = document.querySelector('ol');
 const fontSize = document.querySelector('.fontSize');
+
+const valueFontSize = document.getElementById("valueFontSize");
 
 const getQuestionItem = (questionElement) => questionElement.querySelector('.question-item');
 const getButtonForAnswer = (questionElement) => questionElement.querySelector('.button-answer');
@@ -24,24 +28,25 @@ const getNavigationElement = (navigationElement) => navigationElement.querySelec
 
 function addQuestions() {
   for (let i = 0; i < data.length; ++i) {
-    const questionElement = questionTemplate.cloneNode(true);
+    const questionElement = document.importNode(questionTemplate, true);
 
-    getQuestion(questionElement).innerHTML = data[i].question;
+    getQuestion(questionElement).innerHTML = `${i + 1}. ` + data[i].question;
     getAnswer(questionElement).innerHTML = data[i].answer;
 
     getQuestionItem(questionElement).id = `question-${i + 1}`;
     getButtonForAnswer(questionElement).id = `button-${i + 1}`;
     getButtonForAnswer(questionElement).addEventListener('click', openAnswer);
-
     getPre(questionElement).style.fontSize = '16px';
 
-    function editFontSize() {      // вооот здесь проблема, не работает оно как я хочу
-      // getPre(questionElement).style.fontSize = `${fontSize.value}px`
-      console.log(`${fontSize.value}px`);
+    function editFontSize() {
+      getQuestion(questionElement).style.fontSize = `${fontSize.value}px`;
+      getAnswer(questionElement).style.fontSize = `${fontSize.value}px`;
+      getButtonForAnswer(questionElement).style.fontSize = `${fontSize.value}px`;
+      getQuestion(questionElement).style.lineHeight = `${fontSize.value * 1.2}px`;
+      getAnswer(questionElement).style.lineHeight = `${fontSize.value * 1.2}px`;
     }
 
     fontSize.addEventListener('input', editFontSize);
-
     questionList.append(questionElement);
   }
 }
@@ -61,5 +66,23 @@ function addNavigation() {
 
 addQuestions();
 addNavigation();
+
+// ниже стилизация инпута
+const updateValue = (value) => valueFontSize.innerHTML = `Размер шрифта ${value}`;
+const updateVar = (value) => {
+  fontSize.style.setProperty("--range", (value - minFontValue) * coefficient + "%");
+}
+
+const init = (value) => {
+  valueFontSize.innerHTML = `Размер шрифта ${value}`;
+  updateVar(value);
+}
+
+init(fontSize.value);
+
+fontSize.addEventListener("input", () => {
+  updateVar(fontSize.value);
+  updateValue(fontSize.value);
+});
 
 document.getElementById('consoleClear').addEventListener('click', console.clear);
